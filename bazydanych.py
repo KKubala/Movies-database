6ba6586c60e69c1bc3c3fd0c86fa6831597b3c64
KKubala <73669707+KKubala@users.noreply.github.com>
2020-11-26 20:26:32 +0100

    Update bazydanych.py

--- bazydanych.py
+++ bazydanych.py
@@ -1,14 +1,15 @@
 from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
-from sys import argv
 from tkinter import *
 from narzedzia import *
+
 
 class Osoby(Baza):
     __tablename__ = "osoby"
     id = Column('id', Integer, primary_key=True)
     nazwisko = Column('nazwisko', String, unique=False)
+
 
 class Film(Baza):
     __tablename__ = 'film'
@@ -16,19 +17,22 @@ class Film(Baza):
     tytul = Column('tytul', String, unique=True)
     rok = Column('rok_produkcji', String(4))
     rezyser = Column('rezyser', ForeignKey('osoby.id'))
-    operator = Column('operator', ForeignKey('osoby.id'))
-    producent = Column('producent', ForeignKey('osoby.id'))
-  
-def dodaj_osobe(nazwisko):
-    osoba = Osoby(nazwisko=nazwisko)
-    session.add(osoba)
-    session.commit()
 
-def dodaj_film(tytul, rok, rezyser, operator, producent):
+
+def dodaj_osobe(nazwisko):
+    if nazwisko not in [os.nazwisko for os in session.query(Osoby).order_by(Osoby.nazwisko).all()]:
+        osoba = Osoby(nazwisko=nazwisko)
+        session.add(osoba)
+        session.commit()
+        return "Dodano pomyślnie twórce o nazwisku: " + nazwisko
+    else:
+        return "Ta osoba jest już w bazie: " + nazwisko
+
+
+
+def dodaj_film(tytul, rok, rezyser):
     rezyser_filmu = session.query(Osoby).filter_by(nazwisko=rezyser).first()
-    operator_filmu = session.query(Osoby).filter_by(nazwisko=operator).first()
-    producent_filmu = session.query(Osoby).filter_by(nazwisko=producent).first()
-    film = Film(tytul=tytul,rok=rok, rezyser=rezyser_filmu.id, operator=operator_filmu.id, producent=producent_filmu.id)
+    film = Film(tytul=tytul, rok=rok, rezyser=rezyser_filmu.id)
     session.add(film)
     session.commit()
 
